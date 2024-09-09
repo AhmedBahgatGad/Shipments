4a4c627555c6af179e07cc83779ee3d4a4c28220
Tasneem-3atif <tasneem.atif.for.pc@gmail.com>
2024-09-09 14:16:11 +0300

    task1,2,3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { TableEmployeesComponent } from './components/table-employees/table-employees.component';
+import { LoginComponent } from './components/login/login.component';
+import { AddemployeeComponent } from './components/addemployee/addemployee.component';
+import { AddcityComponent } from './components/addcity/addcity.component';
 
 export const routes: Routes = [
-    {path:'',redirectTo:'home',pathMatch:'full'},
-    {path:'home',component:HomeComponent}
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'employees', component: TableEmployeesComponent },
+  { path: 'addemployee', component: AddemployeeComponent },
+  { path: 'addcity', component: AddcityComponent },
 ];
---
+++ src/app/components/addcity/addcity.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addcity',
+  standalone: true,
+  imports: [],
+  templateUrl: './addcity.component.html',
+  styleUrl: './addcity.component.css'
+})
+export class AddcityComponent {
+
+}
