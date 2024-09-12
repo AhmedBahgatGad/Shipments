cc18d7879a860e353dcc4c1efe8785588451ee28
Sehly <omarsehly@gmail.com>
2024-09-13 02:35:43 +0300

    Gpvernmetns , grous , orders created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { LoginComponent } from "./components/login/login.component";
+import { GovernmentsViewComponent } from "./components/Governments/governments-view/governments-view.component";
+import { OrdersViewComponent } from "./components/orders/orders-view/orders-view.component";
+import { GroupsViewComponent } from "./components/groups/groups-view/groups-view.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, LoginComponent],
+  imports: [RouterOutlet, SidebarComponent, LoginComponent, GovernmentsViewComponent, OrdersViewComponent, GroupsViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
