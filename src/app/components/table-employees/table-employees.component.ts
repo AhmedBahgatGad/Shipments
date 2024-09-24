a9cfb71ff153a04d72fe57c273c68d23ac376923
AhmedBahgatGad <firlongad@gmail.com>
2024-09-24 22:41:33 +0300

    Tasneem branch edit

--- src/app/components/table-employees/table-employees.component.ts
+++ src/app/components/table-employees/table-employees.component.ts
@@ -10,16 +10,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './table-employees.component.css',
 })
 export class TableEmployeesComponent implements OnInit {
-<<<<<<< HEAD
   filteredData: any;
   usersData: any;
   searchTerm: string = '';
-=======
-
-  filteredData: any;
-  usersData: any;
-  searchTerm:string='';
->>>>>>> 11b4bfc5f2dd336a363ccbde5a495b39027b9db1
   constructor(private _UsersService: UsersService) {}
 
   employees: any[] = [];
@@ -27,12 +20,7 @@ export class TableEmployeesComponent implements OnInit {
     this._UsersService.getUsers().subscribe({
       next: (response) => {
         this.employees = response.data;
-<<<<<<< HEAD
         this.filteredData = [...this.employees];
-=======
-        this.filteredData = [...this.employees]
->>>>>>> 11b4bfc5f2dd336a363ccbde5a495b39027b9db1
-        // console.log(response.data);
       },
       error: (err) => {},
     });
